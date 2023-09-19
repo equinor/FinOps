14d6c1e54e57580ca307b305903e7a8b14486f1f
Tobias Carcary Nygaard (Experis AS) <TCNY@equinor.com>
2023-09-19 10:41:41 +0200

    test

--- powershell/CreateWorkspaceParameterFile.ps1
+++ powershell/CreateWorkspaceParameterFile.ps1
@@ -1,8 +1,15 @@
 
 param (
-    [string]$ParameterPath
+    [string]$ParameterPath,
+    [string]$WorkspaceTemplateParamaterPath
 )
 
-$json = Get-Content -Path $ParameterPath -Raw
+$connectionString = "Integrated Security=False;Encrypt=True;Connection Timeout=30;Data Source=tcp:s037-cost-management.sql.azuresynapse.net,1433;Initial Catalog=@{linkedService().DBName}"
+$connectionString.replace("s037-cost-management", "finops-synapse-dev")
+
+Write-Host $connectionString
+
+$synapseParams = Get-Content -Path $ParameterPath -Raw | ConvertFrom-Json
+$workspaceTemplateParams = Get-Content -Path WorkspaceTemplateParamaterPath -Raw | ConvertFrom-Json
 
 Write-Host $json
