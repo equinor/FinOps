56bfa999411a8b31953272496ca49153c948bdb8
Tobias Carcary Nygaard (Experis AS) <TCNY@equinor.com>
2023-09-19 10:54:48 +0200

    test

--- powershell/CreateWorkspaceParameterFile.ps1
+++ powershell/CreateWorkspaceParameterFile.ps1
@@ -4,12 +4,14 @@ param (
     [string]$WorkspaceTemplateParamaterPath
 )
 
-$connectionString = "Integrated Security=False;Encrypt=True;Connection Timeout=30;Data Source=tcp:s037-cost-management.sql.azuresynapse.net,1433;Initial Catalog=@{linkedService().DBName}"
-$connectionString.replace("s037-cost-management", "finops-synapse-dev")
+# $connectionString = "Integrated Security=False;Encrypt=True;Connection Timeout=30;Data Source=tcp:s037-cost-management.sql.azuresynapse.net,1433;Initial Catalog=@{linkedService().DBName}"
+# $connectionString.replace("s037-cost-management", "finops-synapse-dev")
 
-Write-Host $connectionString
+# Write-Host $connectionString
 
 $synapseParams = Get-Content -Path $ParameterPath -Raw | ConvertFrom-Json
-$workspaceTemplateParams = Get-Content -Path WorkspaceTemplateParamaterPath -Raw | ConvertFrom-Json
+$workspaceTemplateParams = Get-Content -Path $WorkspaceTemplateParamaterPath -Raw | ConvertFrom-Json
 
-Write-Host $json
+foreach ($parameter in $workspaceTemplateParams.parameters) {
+    Write-Host $parameter
+}
