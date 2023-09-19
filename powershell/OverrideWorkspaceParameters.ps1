1c670a3e7e268b6f4f657797336a95c1182fb719
tobiasny <tcny@equinor.com>
2023-09-19 16:17:22 +0200

    override spark pool resource parameters

--- powershell/OverrideWorkspaceParameters.ps1
+++ powershell/OverrideWorkspaceParameters.ps1
@@ -10,6 +10,11 @@ $bicepParams = Get-Content -Path $BicepParameterPath -Raw | ConvertFrom-Json
 $sparkPoolNameKey = "sparkPoolName"
 $sparkPoolIdKey = "sparkPoolId"
 $sparkPoolEndpointKey = "sparkPoolEndpoint"
+$sparkPoolNodeSizeKey = "nodeSize"
+$sparkPoolMinNodeCountKey = "sparkPoolMinNodeCount"
+$sparkPoolMaxNodeCountKey = "sparkPoolMaxNodeCount"
+$sparkPoolDelayInMinutesKey = "sparkPoolDelayInMinutes"
+$sparkVersionKey = "sparkVersion"
 
 # Retrieve Synapse workspace template parameters
 $workspaceTemplateParams = Get-Content -Path $WorkspaceTemplateParamaterPath -Raw | ConvertFrom-Json
@@ -26,20 +31,56 @@ foreach ($parameter in $workspaceTemplateParams.parameters.PSObject.Properties) 
 
     if ($parameterKey -like "s037-cost-management*") {
         $workspaceTemplateParams.parameters.$parameterKey.value = $parameterValue.replace("s037-cost-management", $WorkspaceName)
+        continue
     }
 
     # Override spark pool references in notebook artifacts
 
     if ($parameterKey -like "*notebookSparkPoolNameRef") {
         $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolNameKey.value
+        continue
     }
 
     if ($parameterKey -like "*notebookSparkPoolIdRef") {
         $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolIdKey.value
+        continue
     }
 
     if ($parameterKey -like "*notebookSparkPoolEndpointRef") {
         $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolEndpointKey.value
+        continue
+    }
+
+    # Override spark pool resource parameters
+
+    if ($parameterKey -like "*sparkPoolResourceName") {
+        $workspaceTemplateParams.parameters.$parameterKey.value = "$WorkspaceName/$($bicepParams.parameters.$sparkPoolNameKey.value)"
+        continue
+    }
+
+    if ($parameterKey -like "*_delayInMinutes") {
+        $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolDelayInMinutesKey.value
+        continue
+    }
+
+    if ($parameterKey -like "*_maxNodeCount") {
+        $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolMaxNodeCountKey.value
+        continue
+    }
+
+    if (($parameterKey -like "*_minNodeCount") -or ($parameterKey -like "*_nodeCount")) {
+        $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolMinNodeCountKey.value
+        continue
+    }
+
+    if ($parameterKey -like "*_nodeSize") {
+        $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkPoolNodeSizeKey.value
+        continue
+    }
+
+    if ($parameterKey -like "*_sparkVersion") {
+        $workspaceTemplateParams.parameters.$parameterKey.value = $bicepParams.parameters.$sparkVersionKey.value
+        continue
     }
 }
 
